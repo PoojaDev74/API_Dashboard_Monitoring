91489e3a2e6b87908b6f10eebc8fb49e794c964a
Pooja D <poojapreethi2001@gmail.com>
2025-10-04 00:59:47 +0530

    Update StatusCard.jsx

--- client/src/components/StatusCard.jsx
+++ client/src/components/StatusCard.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import "../style/StatusCard.css";
 
-const getColor = (code) => {
-  if (code >= 200 && code < 300) return "green";
-  if (code >= 300 && code < 400) return "orange";
-  if (code >= 400 && code < 600) return "red";
-  return "yellow";
+function StatusCard({ log }) {
+  const getStatus = (status) => {
+    if (status >= 200 && status<300  ) return "green";
+    if (status >= 300 && status < 400) return "orange"; 
+    if (status >= 400 && status < 600) return "red";
+    if (status >= 100 && status < 200) return "yellow"; 
+    return ""; 
 };
-
-export default function StatusCard({ apiName, statuses }) {
-  const latest = statuses.length > 0 ? statuses[statuses.length - 1].statusCode : 200;
 
   return (
     <div className="status-card">
-      <h4>{apiName}</h4>
-      <div className="status-blocks">
-        {statuses.map((s, i) => (
-          <span
-            key={i}
-            className={`dot ${getColor(s.statusCode)}`}
-            title={`${new Date(s.timestamp).toLocaleString()} → ${s.statusCode}`}
-          />
-        ))}
-      </div>
-      <div className="latest">
-        Latest: <span className={`dot ${getColor(latest)}`} />
-      </div>
+        <div className={getStatus(log.status)}></div>
     </div>
   );
 }
+
+export default StatusCard;
