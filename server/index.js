bc2838df93a99a652d8d4921b650971f6d93a881
Pooja D <poojapreethi2001@gmail.com>
2025-10-04 14:50:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:5000",
+  "https://app.netlify.com/projects/apistatusmonitoringdashboard"
      
 ];
 
