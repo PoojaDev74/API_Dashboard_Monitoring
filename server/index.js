1b88cae9b2c11c6b380d16a4f70089abf7d3de86
Pooja D <poojapreethi2001@gmail.com>
2025-10-04 12:27:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import logRoutes from "./routes/logRoutes.js";
+import tracerRoutes from "./routes/tracerRoutes.js";
 import controlRoutes from "./routes/controlRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
 import statusRoutes from "./routes/statusRoutes.js";
-import statsRoutes from "./routes/statsRoutes.js";
 import uptimeRoutes from "./routes/uptimeRoutes.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -14,37 +18,37 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:5000",
-  "https://apidashbord.netlify.app"
+  "https://apidashbord.netlify.app"   
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  allowedHeaders: ["Content-Type", "x-api-key"],
-};
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    allowedHeaders: ["Content-Type", "x-api-key"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 
-app.use(cors(corsOptions));
+app.options("*", cors());
 
+// Middleware
 app.use(express.json());
-connectDB();
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 
 // Routes
-app.use("/api/tracer", logRoutes);
+app.use("/api/tracer", tracerRoutes);
 app.use("/api/controls", controlRoutes);
+app.use("/api/stats", analyticsRoutes);
 app.use("/api/status", statusRoutes);
 app.use("/api/uptime", uptimeRoutes);
-app.use("/api/stats", statsRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
